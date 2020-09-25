7b4dc5d83321b890b87cd74cc5e4a5aef6112e74
nomad10101 <40078206+nomad10101@users.noreply.github.com>
2020-09-24 23:58:38 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,6 +20,8 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 
+// swift-tools-version:5.3
+
 import PackageDescription
 
 let package = Package(
