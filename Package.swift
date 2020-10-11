64a90b3925310a66ad707b578210ffead5d80289
Sami Malik <sami2065@gmail.com>
2020-10-10 23:07:19 -0700

    ..

--- Package.swift
+++ Package.swift
@@ -27,7 +27,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftIcons",
-            dependencies: []),
+            dependencies: [],
+            path: "./Source"),
         .testTarget(
             name: "SwiftIconsTests",
             dependencies: ["SwiftIcons"]),
